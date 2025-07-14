ce22637456c50590594149ce97a272a7b5e7ab53
Aya-Ajamieh <ayaaj778@gmail.com>
2025-07-15 02:35:49 +0300

    حل تعارض في composer.json

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,35 @@
-// import './App.css';
-import LoginRegister from './Components/LoginRegister/LoginRegister';
+// import LoginRegister from './pages/LoginRegister/LoginRegister';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div>
-      <LoginRegister/>
+    <div className='px-3 px-sm-4 px-md-5 px-lg-5'>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/collection' element={<Collection/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/product/:productId' element={<Product/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/place-order' element={<PlaceOrder/>}/>
+            <Route path='/orders' element={<Orders/>}/>
+        </Routes>
+        <Footer/>
+      {/* <LoginRegister/> */}
     </div>
   );
 }
